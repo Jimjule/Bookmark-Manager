6dfb75345683ef0d7f7525744c0a3c51a4e69b5c
Jimjule <roadknightj@gmail.com>
2019-02-27 14:09:09 +0000

    Refactored bookmark to remove redundant method

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -12,12 +12,6 @@ class Bookmark
     @bookmarks
   end
 
-  def self.create(link, name)
-    bookmark = Bookmark.new(link, name)
-    @bookmarks << bookmark
-    bookmark
-  end
-
   def initialize(link, name)
     @link = link
     @name = name
