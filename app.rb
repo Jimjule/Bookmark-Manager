243209996d4a547a8767f1624b3d604470b7c6e9
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-28 10:30:33 +0000

    fixed bug where it was getting params[:id] instead of params[:delete]

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ class Bookmarks < Sinatra::Base
 
   get '/' do
     @bookmarks = Bookmark.all
+    p "@bookmarks in get / route #{@bookmarks}"
     erb :bookmarks
   end
 
@@ -14,8 +15,8 @@ class Bookmarks < Sinatra::Base
   end
 
   post '/delete' do
-    p params[:id]
-    Bookmark.delete(params[:id])
+    p " params[:delete] in /delete #{params[:delete]} "
+    Bookmark.delete(params[:delete])
     redirect '/'
   end
 
